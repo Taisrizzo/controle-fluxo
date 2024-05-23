a95a98d5640db2c740691c5730246169cb19d824
Tais Rizzo <tais_margo@hotmail.com>
2024-05-22 23:18:51 -0300

    alteração nos comentários

--- src/SistemaMedidaSwitchCase2.java
+++ src/SistemaMedidaSwitchCase2.java
@@ -30,4 +30,6 @@ public class SistemaMedidaSwitchCase2 {
     }
 } /*No código que está dentro do comentário lá em cima dentro do "else if" utilizamos mais linhas de código para imprimir os beneficios dos planos da operadora*/
 
-/*Já no código abaixo usando "switch case" com os planos da operadora do MAIOR para o MENOR, usei menos linhas de código para simplificar o código, e uma linha compor a outra, de acordo com a escolha do plano da operadora*/
+/*Já no código abaixo usando "switch case" com os planos da operadora do MAIOR para o MENOR, usei menos linhas para simplificar o código,*/
+
+//Observe também que,retiramos o "break" de cada condição. Assim poderá ser impresso uma ou mais informações na tela, de acordo com a opção que o usuário escolher, 
